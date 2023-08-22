310138efcc280bc9e008843ba9c038711402c3ef
priyanshu <priyanshu@e6x.o>
2023-08-22 17:22:05 +0530

    producer consumer without queue block

--- src/main/java/io/e6x/SharedQueue.java
+++ src/main/java/io/e6x/SharedQueue.java
@@ -10,29 +10,24 @@ public class SharedQueue {
     int size;
     Semaphore fullCount;
     Semaphore emptyCount;
-    Semaphore accessQueue;
+
     public SharedQueue(int size){
         queue = new LinkedList<>();
         this.size = size;
         this.fullCount = new Semaphore(0);
         this.emptyCount = new Semaphore(size);
-        this.accessQueue = new Semaphore(1);
     }
     public void produce() throws InterruptedException {
-        accessQueue.acquire();
         emptyCount.acquire();
         Random random = new Random();
         queue.add(random.nextInt());
         fullCount.release();
-        accessQueue.release();
     }
     public void consume() throws InterruptedException {
-        accessQueue.acquire();
         fullCount.acquire();
         int value = queue.poll();
         System.out.println(value);
         emptyCount.release();
-        accessQueue.release();
     }
 
 
